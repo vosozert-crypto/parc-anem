484f6bfc5bc292bf9eca311575c5a23fd473087a
hommerasdu-alt <aissalaifaoui21@gmail.com>
2026-08-17 15:23:17 +0100

    Ajout passenger_wsgi.py pour PythonAnywhere

---
+++ passenger_wsgi.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+project_home = os.path.dirname(os.path.abspath(__file__))
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+from app import create_app
+
+application = create_app()
